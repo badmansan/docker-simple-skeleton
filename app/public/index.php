65bbc01b955a6b3bb752d1a59085c7ff47b2d173
Victooor <victorst256@gmail.com>
2021-07-29 17:00:57 +0300

    + doctrine/collections for autoload demo

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,11 @@
 <?php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// test autoload
+$collection = new ArrayCollection(range(10, 20));
+print_r($collection->toArray());
+
 phpinfo();
