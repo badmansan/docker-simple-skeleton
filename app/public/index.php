7b5517637f5f6c5f594bf116f77399d541b86a64
victor <victorst256@gmail.com>
2022-08-01 23:46:54 +0300

    + creating db & dbuser while first start

--- app/public/index.php
+++ app/public/index.php
@@ -9,7 +9,7 @@ $collection = new ArrayCollection(range(10, 20));
 echo '<pre>' . print_r($collection->toArray(), TRUE) . '</pre>';
 
 // test db
-$pdo = new PDO('pgsql:host=db;port=5432;dbname=postgres', 'postgres', 'postgres');
+$pdo = new PDO('pgsql:host=db;port=5432;dbname=app_db', 'app_db_user', '123456');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $stmt = $pdo->query('SELECT NOW()');
