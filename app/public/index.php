f2051d9aab66a8431ff8474abd0fc6e646d0338e
victor <victorst256@gmail.com>
2022-08-01 22:40:52 +0300

    + add docker files to build DEV alpine images

--- app/public/index.php
+++ app/public/index.php
@@ -5,14 +5,21 @@ use Doctrine\Common\Collections\ArrayCollection;
 require __DIR__ . '/../vendor/autoload.php';
 
 // test autoload
-$collection = new ArrayCollection(range(10, 20));
+$collection = new ArrayCollection(range(10, 15));
 echo '<pre>' . print_r($collection->toArray(), TRUE) . '</pre>';
 
 // test db
 $pdo = new PDO('pgsql:host=db;port=5432;dbname=app_db', 'app_db_user', '123456');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 $stmt = $pdo->query('SELECT NOW()');
-echo '<pre>' . print_r($stmt->fetchColumn(), TRUE) . '</pre>';
+echo '<pre>Get date/time using database query...';
+echo $stmt->fetchColumn() . '</pre>';
+
+// test intl extension
+$localeStr = 'uk-UA';
+echo '<pre>' . 'Locale set to `' . $localeStr . '`...';
+$locale = new Locale();
+$isLocaleSetSuccess = $locale::setDefault($localeStr);
+echo ($isLocaleSetSuccess ? 'success' : 'fail') . '</pre>';
 
 phpinfo();
